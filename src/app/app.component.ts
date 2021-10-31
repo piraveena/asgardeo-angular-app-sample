d34b66ba095376ac80b082bf9d18bf25af279acb
piraveena <piraveena18@gmail.com>
2021-10-31 12:39:00 +0530

    Add angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+// app.component.ts
+
+import { Component } from "@angular/core";
+import {AsgardeoAuthService, AuthStateInterface, DecodedIDTokenPayload} from "@asgardeo/auth-angular";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'demo-angular-app';
+
+  public isAuthenticated: boolean = false;
+  public accessToken: String | undefined;
+  public decodedIdToken: DecodedIDTokenPayload | undefined;
+  constructor(private auth: AsgardeoAuthService) { }
+
+  ngOnInit() {
+    this.auth.state$
+      .subscribe((state: AuthStateInterface) => {
+        this.isAuthenticated = state.isAuthenticated;
+      });
+  }
+
+
+  // Use this function in a login button to simply sign-in.
+  handleSignIn(): void {
+    console.log("Add login--");
+    this.auth.signIn();
+  }
+
+  getAccessToken() {
+    this.auth.getAccessToken().then((accessToken) => {
+      console.log("Get Access Token--");
+      this.accessToken = accessToken;
+    });
+  }
+
+  getDecodedIdToken() {
+    this.auth.getDecodedIDToken().then((decodedIdToken) => {
+      console.log("Get ID token--");
+      this.decodedIdToken = decodedIdToken;
+    });
+  }
+
+  handleSignout() {
+    this.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+//app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AsgardeoAuthModule } from "@asgardeo/auth-angular";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AsgardeoAuthModule.forRoot({
+      signInRedirectURL: "http://localhost:4200",
+      signOutRedirectURL: "http://localhost:4200",
+      clientID: "dummy_client_id",
+      scope: ["openid", "email"],
+      serverOrigin: "https://api.asgardeo.io/t/your_prod_here"
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // Add your providers here.
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
